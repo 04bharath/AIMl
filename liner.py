16b3807fc20e4b23ccb7c9a792991c562ba702de
ravi <ravi123@gmail.com>
2023-10-16 14:35:27 +0530

    change 2

--- liner.py
+++ liner.py
@@ -16,10 +16,5 @@ h=y_pred.reshape(21,)
 mydict={"Actual": g,"Pred":h}
 com=pd.DataFrame(mydict)
 com.sample(10)
-def evaluationmatrices(Actual,Pred):
- MAE=mean_absolute_error(Actual,Pred)
- MSE=mean_squared_error(Actual,Pred)
- RMSE=np.sqrt(mean_squared_error(Actual,Pred))
- SCORE=r2_score (Actual,Pred)
- return print ("r2 score:",SCORE,"\n","MAE", MAE,"\n","mse",MSE,"\n","RMSE",RMSE)
+
 
